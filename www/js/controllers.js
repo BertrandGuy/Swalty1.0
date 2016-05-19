63e48d2b404e9b7eabc162da947a42ed4a23ff22
jisooyun <jisooyun1994@gmail.com>
2016-05-19 14:57:33 +0200

    FIREBASE IS WORKIIIIING

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,33 @@
-angular.module('starter').controller('RecetteController', function($scope){
+
+angular.module('SwaltyApp').factory("Recettes", function($firebaseArray) {
+    var itemsRef = new Firebase("https://swaltyapp.firebaseio.com/recettes");
+    return $firebaseArray(itemsRef);
+});
+
+
+angular.module('SwaltyApp').controller("AddRecetteController", function($scope, Recettes) {
+
+    $scope.recettes = Recettes;
+
+    $scope.addRecette = function() {
+        titre = this.titre;
+        description = this.description;
+        difficulte = this.difficulte;
+        temps = this.temps;
+        nbPersonne = this.nbPersonne;
+
+        $scope.recettes.$add({
+            "titre": titre,
+            "description": description,
+            "difficulte": difficulte,
+            "temps": temps,
+            "nbPersonne": nbPersonne
+        });
+
+    };
+});
+
+/* angular.module('SwaltyApp').controller('RecetteController', function($scope){
    $scope.recettes = [
        {id: 1, title: "titre de la recette 1", duree:"3"},
        {id: 2, title: "titre de la recette 2", duree:"20"},
@@ -6,5 +35,12 @@ angular.module('starter').controller('RecetteController', function($scope){
        {id: 4, title: "titre de la recette 4", duree:"4"},
        {id: 5, title: "titre de la recette 5", duree:"15"}
    ]
+}); */
+
+
+
+angular.module("SwaltyApp").controller("FavController", function($scope) {
+
+
 });
 
